e0d40129f70d9e5f6d1868cd6af293fe5eee0628
Matias Servetto <mservetto@despegar.com>
2015-06-28 17:20:18 -0300

    initial

---
+++ DMFlightsFoundation/DMFlightsResultsViewController.m
@@ -0,0 +1,17 @@
+//
+//  DMFlightsResultsViewController.m
+//  DMFlightsFoundation
+//
+//  Created by Matias Servetto on 6/28/15.
+//  Copyright (c) 2015 Despegar.com. All rights reserved.
+//
+
+#import "DMFlightsResultsViewController.h"
+
+@interface DMFlightsResultsViewController ()
+
+@end
+
+@implementation DMFlightsResultsViewController
+
+@end
---
+++ DMFlightsFoundation/DMFlightsSearchViewController.m
@@ -0,0 +1,17 @@
+//
+//  DMFlightsSearchViewController.m
+//  DMFlightsFoundation
+//
+//  Created by Matias Servetto on 6/28/15.
+//  Copyright (c) 2015 Despegar.com. All rights reserved.
+//
+
+#import "DMFlightsSearchViewController.h"
+
+@interface DMFlightsSearchViewController ()
+
+@end
+
+@implementation DMFlightsSearchViewController
+
+@end
