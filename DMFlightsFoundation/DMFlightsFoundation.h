828ac137f87ab1b58d4382ec743c6631169afc73
Matias Servetto <mservetto@despegar.com>
2015-06-28 17:11:55 -0300

    Initial Commit

---
+++ DMFlightsFoundation/DMFlightsFoundation.h
@@ -0,0 +1,19 @@
+//
+//  DMFlightsFoundation.h
+//  DMFlightsFoundation
+//
+//  Created by Matias Servetto on 6/28/15.
+//  Copyright (c) 2015 Despegar.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DMFlightsFoundation.
+FOUNDATION_EXPORT double DMFlightsFoundationVersionNumber;
+
+//! Project version string for DMFlightsFoundation.
+FOUNDATION_EXPORT const unsigned char DMFlightsFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DMFlightsFoundation/PublicHeader.h>
+
+
