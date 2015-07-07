5ef446a43c513fb3acdd2276c90391be794d8605
Nicolas Miyasato <nicolas.miyasato@despegar.com>
2015-07-07 15:21:55 -0300

    Agrego clase en común

--- DMFlightsFoundation/DMFlightsFoundation.h
+++ DMFlightsFoundation/DMFlightsFoundation.h
@@ -17,4 +17,4 @@ FOUNDATION_EXPORT const unsigned char DMFlightsFoundationVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <DMFlightsFoundation/PublicHeader.h>
 
 #import <DMFlightsFoundation/DMFlightsSearchViewController.h>
-
+#import <DMFlightsFoundation/DMInfo.h>
---
+++ DMFlightsFoundation/DMInfo.m
@@ -0,0 +1,17 @@
+//
+//  DMInfo.m
+//  DMFlightsFoundation
+//
+//  Created by Nicolas Miyasato on 7/7/15.
+//  Copyright (c) 2015 Despegar.com. All rights reserved.
+//
+
+#import "DMInfo.h"
+
+@implementation DMInfo
+
+- (void) sayHello {
+    NSLog(@"Hello from Flights");
+}
+
+@end
