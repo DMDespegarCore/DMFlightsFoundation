9b9de51da840dfa34c3b1d7194446efbd50bbb7c
Nicolas Battelli <nbattelli@Nicolass-MacBook-Pro.local>
2015-07-23 12:19:22 -0300

    Pruebas con xSelling y Typhoon

--- DMFlightsFoundation/DMFlightsFoundation.h
+++ DMFlightsFoundation/DMFlightsFoundation.h
@@ -17,4 +17,5 @@ FOUNDATION_EXPORT const unsigned char DMFlightsFoundationVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <DMFlightsFoundation/PublicHeader.h>
 
 #import <DMFlightsFoundation/DMFlightsSearchViewController.h>
+#import <DMFlightsFoundation/DMFlightsResultsViewController.h>
 #import <DMFlightsFoundation/DMInfo.h>
